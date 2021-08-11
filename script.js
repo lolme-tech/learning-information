2689a63a38be9e5f4385f15f643fddaec1c6fdd6
lolme-tech <63903850+lolme-tech@users.noreply.github.com>
2021-08-12 03:59:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var $main=$('#main');
+$('#main').css("font-size","30px");
+$main.css("color","blue");
+$('.describe').css("font-size","20px");
+$(function(){
+    $('.profile').hover(function(){
+        $(this).html("工事中").css("color","red");
+    },function(){
+        $(this).html('自己紹介').css('color','#808080');
+    });
+});
